92920f9eafa7562bee4e69f4d85246b9ae198fe4
Patrycja Patrycja <p.stepien3@student.uw.edu.pl>
2024-03-21 02:29:14 +0100

    initial start page layout

--- warehouse-buddy/src/containers/HomeView/TicketSystem/index.js
+++ warehouse-buddy/src/containers/HomeView/TicketSystem/index.js
@@ -1,19 +1,37 @@
 import './index.css';
-import { Button, useDisclosure, Flex, Spacer, Divider } from '@chakra-ui/react'
+import {
+    Button,
+    useDisclosure,
+    Flex,
+    Spacer,
+    Divider,
+    Card,
+    CardBody
+} from '@chakra-ui/react'
 
-import{Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton} from '@chakra-ui/react'
-import { Textarea } from '@chakra-ui/react'
-import { Select } from '@chakra-ui/react'
-import { useSelector, useDispatch } from 'react-redux';
-import { sendTicket } from '../../../redux/openai_api/actions';
-import React, { useState } from 'react';
-import { WebcamScreenshot } from './WebcameraScreenshot';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton
+} from '@chakra-ui/react'
+import {Textarea} from '@chakra-ui/react'
+import {Select} from '@chakra-ui/react'
+import {useSelector, useDispatch} from 'react-redux';
+import {sendTicket} from '../../../redux/openai_api/actions';
+import React, {useState} from 'react';
+import {WebcamScreenshot} from './WebcameraScreenshot';
 
-export const TicketSystem = () => {
+export const TicketSystem = ({mealType}) => {
     const dispatch = useDispatch();
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [selectedCategory, setSelectedCategory] = useState('');
-    const [issue, setIssue] = useState('');
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const [selectedCategory,
+        setSelectedCategory] = useState('');
+    const [issue,
+        setIssue] = useState('');
 
     const handleChange = (event) => {
         setSelectedCategory(event.target.value);
@@ -23,47 +41,71 @@ export const TicketSystem = () => {
         setIssue(event.target.value);
     }
 
-    let categories = ["Bezpieczeństwo", "Załadunek", "Rozładunek", "Czystość i ergonomia", "Lokalizacja w magazynie", "Inne"]
+    let categories = [
+        "Bezpieczeństwo",
+        "Załadunek",
+        "Rozładunek",
+        "Czystość i ergonomia",
+        "Lokalizacja w magazynie",
+        "Inne"
+    ]
     const list = categories.map((el) => {
         return <option value={el} key={el}>{el}</option>;
     });
 
     const onSend = () => {
-            // Send data to the backend via POST
-            let new_ticket = {
-                "category": selectedCategory,
-                "description": issue
-            }
+        // Send data to the backend via POST
+        let new_ticket = {
+            "category": selectedCategory,
+            "description": issue
+        }
 
-            setIssue('');
+        setIssue('');
 
-            dispatch(sendTicket(new_ticket));
-            onClose();
+        dispatch(sendTicket(new_ticket));
+        onClose();
     }
 
     return (
-        <div class="ticket-system-container">
-            <Button id="ticket-button" onClick={onOpen} colorScheme="red" variant="solid">Report Issue</Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader />
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <WebcamScreenshot />
-                        <Select class="select-ticket-type" placeholder='Wybierz kategorię zgłoszenia'onChange={handleChange} value={selectedCategory}>
-                            {list}
-                        </Select> 
-                        <Divider id="ticket-divider"/>  
-                        <Textarea placeholder='Write your description here.' size='md' h='calc(20vh)' onChange={handleChangeInput} value={issue}/>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='facebook' mr={3} onClick={onSend}>
-                            Submit
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </div>
+        <Card >
+            <CardBody>
+                <Button
+                    id="ticket-button"
+                    onClick={onOpen}
+                    colorScheme='teal'
+                    variant='outline'
+                    size='lg'>{mealType}</Button>
+
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay/>
+                    <ModalContent>
+                        <ModalHeader/>
+                        <ModalCloseButton/>
+                        <ModalBody>
+                            <WebcamScreenshot/>
+                            <Select
+                                class="select-ticket-type"
+                                placeholder='Wybierz kategorię zgłoszenia'
+                                onChange={handleChange}
+                                value={selectedCategory}>
+                                {list}
+                            </Select>
+                            <Divider id="ticket-divider"/>
+                            <Textarea
+                                placeholder='Write your description here.'
+                                size='md'
+                                h='calc(20vh)'
+                                onChange={handleChangeInput}
+                                value={issue}/>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme='facebook' mr={3} onClick={onSend}>
+                                Submit
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </CardBody>
+        </Card>
     );
 }
--- warehouse-buddy/src/containers/HomeView/index.js
+++ warehouse-buddy/src/containers/HomeView/index.js
@@ -1,28 +1,52 @@
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import { ChatInput } from './ChatInput';
-import { FastPromptSection } from './FastPromptSection';
-import { TaskBanner } from './TaskBanner';
-import { SpeechToText } from './ChatInput/SpeechToText';
-import { Divider, Stack } from '@chakra-ui/react'
+import {ChatInput} from './ChatInput';
+import {FastPromptSection} from './FastPromptSection';
+import {TaskBanner} from './TaskBanner';
+import {SpeechToText} from './ChatInput/SpeechToText';
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    CardFooter,
+    Heading,
+    Divider,
+    Stack,
+    Text,
+    Box
+} from '@chakra-ui/react'
 
 import './index.css';
-import { TicketSystem } from './TicketSystem';
+import {TicketSystem} from './TicketSystem';
 
-export const HomeView = ({ tasks }) => {
+export const HomeView = ({tasks}) => {
     return (
-        <div class="home-view-container">
-            <Stack spacing={4}>
-                <TaskBanner tasks={tasks} />
-                <TicketSystem />
-                <div class="chat-container">
-                    <FastPromptSection tasks={tasks} />
-                    <Divider id="chat-divider"/>
-                    <ChatInput tasks={tasks} />
-                </div>
-            </Stack>
-        </div>
+        <Card overflow='hidden' variant='outline' >
+            <CardHeader>
+                <Heading size='md'>Your diet</Heading>
+            </CardHeader>
+            <CardBody>
+                <Stack spacing={4}>
+                    <Card direction='row' justifyContent="space-around">
+                        <Card>
+                            <CardBody>
+                                <Button id="ticket-button" colorScheme='blue' variant='solid' size='lg'>Water!</Button>
+                            </CardBody>
+                        </Card>
+                        <TicketSystem mealType={"snack"}/>
+                    </Card>
+                    <TicketSystem mealType={"Add a meal"}/>
+                </Stack>
+            </CardBody>
+            <CardFooter>
+                <Text>
+                    Water: 5 / 8</Text>
+                <br/>
+                <Text>
+                    Your calorie intake: 1200 / 1700</Text>
+            </CardFooter>
+        </Card>
     );
 }
