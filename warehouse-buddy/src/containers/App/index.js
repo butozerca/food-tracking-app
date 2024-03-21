d37e844f87062e483c1941e848a20fa90e1959e3
Michal Dyrek <michaldyrek@Michals-MacBook-Air.local>
2024-03-21 05:15:47 +0100

    Comment out erroring request

--- warehouse-buddy/src/containers/App/index.js
+++ warehouse-buddy/src/containers/App/index.js
@@ -8,21 +8,21 @@ import { ChakraProvider } from '@chakra-ui/react'
 const App = () => {
     const [tasks, setTasks] = useState([])
 
-    const fetchTaskData = () => {
-        fetch("http://localhost:5000/tasks")
-          .then(response => {
-            return response.json()
-          })
-          .then(data => {
-            setTasks(data); 
-          })
-    }
+    // const fetchTaskData = () => {
+    //     fetch("http://localhost:5000/tasks")
+    //       .then(response => {
+    //         return response.json()
+    //       })
+    //       .then(data => {
+    //         setTasks(data); 
+    //       })
+    // }
 
-    useEffect(() => {
-        fetchTaskData()
-    }, [])
+    // useEffect(() => {
+    //     fetchTaskData()
+    // }, [])
 
-    console.log(tasks);
+    // console.log(tasks);
 
     return (
         <ChakraProvider>
