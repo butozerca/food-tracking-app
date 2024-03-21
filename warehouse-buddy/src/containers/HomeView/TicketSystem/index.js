e813d15bf832ad3f78f7dd404f39fe3f13226c5c
Anna Stawiska <ania.stawiska@gmail.com>
2024-03-21 12:43:34 +0100

    Capture picture and trigger listen animation

--- warehouse-buddy/src/containers/HomeView/TicketSystem/index.js
+++ warehouse-buddy/src/containers/HomeView/TicketSystem/index.js
@@ -6,8 +6,9 @@ import { Textarea } from '@chakra-ui/react'
 import { Select } from '@chakra-ui/react'
 import { useSelector, useDispatch } from 'react-redux';
 import { sendTicket } from '../../../redux/openai_api/actions';
-import React, { useState } from 'react';
-import { WebcamScreenshot } from './WebcameraScreenshot';
+import React, { useState, useCallback, useRef } from 'react';
+// import { WebcamScreenshot } from './WebcameraScreenshot';
+import Webcam from "react-webcam"
 
 export const TicketSystem = () => {
     const dispatch = useDispatch();
@@ -27,6 +28,44 @@ export const TicketSystem = () => {
     // const list = categories.map((el) => {
     //     return <option value={el} key={el}>{el}</option>;
     // });
+
+    const [img, setImg] = useState(null);
+    const webcamRef = useRef(null);
+
+    const videoConstraints = {
+        width: 420,
+        height: 420,
+        facingMode: "user",
+    };
+
+    // randomly mocked each image
+    let user_id = 0 + Math.random() * (1000000 - 0);
+
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImg(imageSrc);
+
+        let apiUrl = 'http://localhost:5000/save_image';
+        let imageData = imageSrc.replace(/^data:image\/png;base64,/, '');
+        let req = {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'No-Auth': 'True'
+        },
+        body: JSON.stringify({ image_data: imageData, id: user_id }),
+        }
+        user_id = user_id + 1;
+        fetch(apiUrl, req)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }, [webcamRef]);
 
     const onSend = () => {
             // Send data to the backend via POST
@@ -50,18 +89,42 @@ export const TicketSystem = () => {
                     <ModalHeader />
                     <ModalCloseButton />
                     <ModalBody>
-                        <WebcamScreenshot />
+                        <div className="webcam-container">
+                            {img === null ? (
+                                <>
+                                    <Webcam
+                                        audio={false}
+                                        mirrored={true}
+                                        height={400}
+                                        width={400}
+                                        ref={webcamRef}
+                                        screenshotFormat="image/jpeg"
+                                        videoConstraints={videoConstraints}
+                                    />
+                                    <div class="photo-button">
+                                        <Button class="capture-button" onClick={capture}><div class="button">Capture</div></Button>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                <img src={img} alt="screenshot" />
+                                <div class="photo-button">
+                                    <Button class="listen-button" onClick={() => setImg(null)}><div class="button"><div class="dot-pulse"></div></div><div class="button">Done</div></Button>
+                                </div>
+                                </>
+                            )}
+                        </div>
                         {/* <Select class="select-ticket-type" placeholder='Wybierz kategorię zgłoszenia'onChange={handleChange} value={selectedCategory}>
                             {list}
-                        </Select> 
-                        <Divider id="ticket-divider"/>  
+                        </Select>  */}
+                        {/* <Divider id="ticket-divider"/>  
                         <Textarea placeholder='Write your description here.' size='md' h='calc(20vh)' onChange={handleChangeInput} value={issue}/> */}
                     </ModalBody>
-                    {/* <ModalFooter>
-                        <Button colorScheme='facebook' mr={3} onClick={onSend}>
-                            Submit
-                        </Button>
-                    </ModalFooter> */}
+                    <ModalFooter>
+                        {/* <Button colorScheme='facebook' mr={3} onClick={capture}>
+                            Picture
+                        </Button> */}
+                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </div>
